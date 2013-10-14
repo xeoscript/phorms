2b4c1880160eb46c6941c8b27db17e8ccd9717da
ajumell <ajumell@xeoscript.com>
2013-10-14 21:22:11 +0530

    Added option to include required files for wordpress.

---
+++ wordpress.php
@@ -0,0 +1,8 @@
+<?php
+
+define('PHORMS_WORDPRESS', true);
+
+include(dirname(__FILE__) . '/' . 'phorms.php');
+
+include(PHORMS_ROOT . 'WordpressPhorms/AbstractWordpressPhorm.class.php');
+include(PHORMS_ROOT . 'WordpressPhorms/WordpressPostMetaPhorm.class.php');
